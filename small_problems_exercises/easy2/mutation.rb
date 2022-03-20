990945a2e6c53167dbeca4f0ae48d0bb7cab37a9
Julien Devlin <juliendevlin@gmail.com>
2022-03-20 12:04:05 -0400

    made small change to mutation.rb

--- small_problems_exercises/easy2/mutation.rb
+++ small_problems_exercises/easy2/mutation.rb
@@ -3,3 +3,11 @@ array2 = []
 array1.each { |value| array2 << value }
 array1.each { |value| value.upcase! if value.start_with?('C', 'S') }
 puts array2
+# Moe
+# Larry
+# CURLY
+# SHEMP
+# Harpo
+# CHICO
+# Groucho
+# Zeppo
